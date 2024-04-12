4069a9e8074c57e573c313bf2960aac82902c4ff
-3σ <yurippe@m3sigma.net>
2024-04-12 09:33:41 +0900

    linear init

---
+++ model/__init__.py
@@ -0,0 +1,8 @@
+from .configuration_linear_transformer import LinearTransformerConfig, LinearTransformerOnnxConfig
+from .linear_transformer import (
+    LinearTransformerModel,
+    LinearTransformerForCausalLM,
+    LinearTransformerForConditionalGeneration,
+    LinearTransformerForQuestionAnswering,
+    LinearTransformerForSequenceClassification
+)
